f877911b5791bb7f607fe6796c6f2ae7d46b5cb1
Felix Icaza <fx.joliett17@gmail.com>
2022-02-22 19:50:00 -0600

    create filter transportTypes

---
+++ build/js/filter.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+/* eslint-disable no-plusplus */
+
+const formFilter = document.getElementById('filter')
+const radioAll = document.getElementById('all')
+const radioExpress = document.getElementById('express')
+const radioRouted = document.getElementById('routed')
+
+const expressBuses = document.querySelectorAll('.expreso')
+const routedBuses = document.querySelectorAll('.ruteado')
+
+// TODO: Borrar cuándo sea necesario
+// Obtener la cantidad de títulos de los buses expresos y ruteados
+const transportTitles = document.querySelectorAll('.titles')
+const expressTitles = document.querySelectorAll('.expreso > .titles')
+const routedTitles = document.querySelectorAll('.ruteado > .titles')
+
+formFilter.addEventListener('change', e => {
+  if (e.target.id === 'all' && radioAll.checked) {
+    // TODO: Borrar cuándo sea necesario
+    // Actualizar el número del título de todos los buses cuándo sean visibles
+    transportTitles.forEach(
+      (title, index) => (title.textContent = `Horario #${index + 1}`)
+    )
+
+    expressBuses.forEach(express => express.classList.remove('hidden'))
+    routedBuses.forEach(routed => routed.classList.remove('hidden'))
+  }
+
+  if (e.target.id === 'express' && radioExpress.checked) {
+    // TODO: Borrar cuándo sea necesario
+    // Actualizar el número del título de los buses expresos cuándo éstos sean visibles
+    for (let i = 0; i < expressTitles.length; i++) {
+      expressTitles[i].textContent = `Horario Expresos #${i + 1}`
+    }
+
+    expressBuses.forEach(express => express.classList.remove('hidden'))
+    routedBuses.forEach(routed => routed.classList.add('hidden'))
+  }
+
+  if (e.target.id === 'routed' && radioRouted.checked) {
+    // TODO: Borrar cuándo sea necesario
+    // Actualizar el número del título de los buses ruteados cuándo éstos sean visibles
+    for (let i = 0; i < routedTitles.length; i++) {
+      routedTitles[i].textContent = `Horario Ruteados #${i + 1}`
+    }
+
+    routedBuses.forEach(routed => routed.classList.remove('hidden'))
+    expressBuses.forEach(express => express.classList.add('hidden'))
+  }
+})
