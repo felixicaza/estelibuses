9e962448199e7d4a2b962dde0cc209e4ed97bd1f
Felix Icaza <fx.joliett17@gmail.com>
2022-03-02 20:19:00 -0600

    add functionality for header search

---
+++ build/js/header-search.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-plusplus */
+
+const mainSearch = document.getElementById('main-search')
+const citiesSearch = document.getElementById('cities-search')
+
+const containerCities = document.getElementById('container-cities')
+const cities = document.querySelectorAll('.cities')
+const citiesName = document.querySelectorAll('.cities > a span')
+
+const unknownCity = document.getElementById('unknown-city')
+
+mainSearch.addEventListener('input', e => {
+  if (e.target.id === 'cities-search') {
+    containerCities.style.opacity = '1'
+    containerCities.style.transform = 'translateY(0)'
+    containerCities.style.pointerEvents = 'auto'
+
+    const sanitizeInput = citiesSearch.value
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/\s/g, '-')
+
+    for (let i = 0; i < citiesName.length; i++) {
+      const name = citiesName[i].textContent || citiesName[i].innerText
+      const sanitizeName = name
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/\s\(\w{8}\s\w{5}\)/, '')
+        .replace(/\s\(\w{8}\s\w{3}\)/, '')
+        .replace(/\s/g, '-')
+
+      if (sanitizeName.indexOf(sanitizeInput) > -1) {
+        cities[i].classList.remove('hidden')
+      } else {
+        cities[i].classList.add('hidden')
+      }
+    }
+
+    if (document.querySelectorAll('.cities.hidden').length === cities.length) {
+      unknownCity.classList.remove('hidden')
+    } else {
+      unknownCity.classList.add('hidden')
+    }
+
+    if (citiesSearch.value === '') {
+      containerCities.style.opacity = '0'
+      containerCities.style.transform = 'translateY(30%)'
+      containerCities.style.pointerEvents = 'none'
+    }
+  }
+})
+
+mainSearch.addEventListener('submit', e => e.preventDefault())
