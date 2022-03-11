5b5a6dbe35876cdf0b69ac8bb33e0677de2eee2c
Felix Icaza <fx.joliett17@gmail.com>
2022-03-11 16:33:37 -0600

    create app shell

---
+++ build/js/sw-app-shell-media.js
@@ -0,0 +1,137 @@
+/* eslint-disable no-unused-vars */
+
+const IMAGES_APP_SHELL = [
+  '/img/anuncios/real-estate-mobile.avif',
+  '/img/anuncios/real-estate-mobile.webp',
+  '/img/anuncios/real-estate-mobile.jpg',
+  '/img/anuncios/real-state-desktop.avif',
+  '/img/anuncios/real-estate-desktop.webp',
+  '/img/anuncios/real-estate-desktop.jpg',
+
+  '/favicons/favicon-16x16.png',
+  '/favicons/favicon-32x32.png',
+  '/favicons/favicon-48x48.png',
+  '/favicons/android-chrome-144x144.png',
+
+  '/img/esteli-buses-logo-black-felix-icaza.webp',
+  '/img/esteli-buses-logo-black-felix-icaza.png',
+  '/img/esteli-buses-logo-white-felix-icaza.webp',
+  '/img/esteli-buses-logo-white-felix-icaza.png',
+
+  '/img/esteli-buses-bg-dark-felix-icaza.avif',
+  '/img/esteli-buses-bg-dark-felix-icaza.webp',
+  '/img/esteli-buses-bg-dark-mobile-felix-icaza.avif',
+  '/img/esteli-buses-bg-dark-mobile-felix-icaza.webp',
+
+  '/img/esteli-buses-bg-felix-icaza.avif',
+  '/img/esteli-buses-bg-felix-icaza.webp',
+  '/img/esteli-buses-bg-mobile-felix-icaza.avif',
+  '/img/esteli-buses-bg-mobile-felix-icaza.webp',
+
+  '/img/terminal-norte-estelibuses-felix-icaza.avif',
+  '/img/terminal-norte-estelibuses-felix-icaza.webp',
+  '/img/terminal-norte-estelibuses-mobile-felix-icaza.avif',
+  '/img/terminal-norte-estelibuses-mobile-felix-icaza.webp',
+
+  '/img/terminal-sur-estelibuses-felix-icaza.avif',
+  '/img/terminal-sur-estelibuses-felix-icaza.webp',
+  '/img/terminal-sur-estelibuses-mobile-felix-icaza.avif',
+  '/img/terminal-sur-estelibuses-mobile-felix-icaza.webp',
+
+  '/img/error-horarios.jpg',
+
+  '/img/404.avif',
+  '/img/404.webp',
+  '/img/404.jpg',
+
+  '/img/offline.avif',
+  '/img/offline.webp',
+  '/img/offline.jpg',
+
+  '/img/terminal-norte/horarios-esteli-achuapa-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-achuapa-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-el-regadio-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-el-regadio-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-el-sauce-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-el-sauce-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-jalapa-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-jalapa-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-jinotega-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-jinotega-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-la-concordia-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-la-concordia-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-la-pita-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-la-pita-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-managua-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-managua-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-masaya-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-masaya-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-ocotal-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-ocotal-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-pueblo-nuevo-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-pueblo-nuevo-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-quilali-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-quilali-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-san-juan-de-limay-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-san-juan-de-limay-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-san-juan-del-rio-coco-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-san-juan-del-rio-coco-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-san-rafael-del-norte-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-san-rafael-del-norte-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-somoto-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-somoto-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-wiwili-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-wiwili-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-norte/horarios-esteli-yali-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-norte/horarios-esteli-yali-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-achuapa-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-achuapa-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-el-espinal-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-el-espinal-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-la-trinidad-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-la-trinidad-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-leon-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-leon-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-managua-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-managua-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-matagalpa-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-matagalpa-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-murra-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-murra-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-san-nicolas-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-san-nicolas-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-tisey-la-estanzuela-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-tisey-la-estanzuela-estelibuses-felix-icaza-card.webp',
+
+  '/img/terminal-sur/horarios-esteli-yali-estelibuses-felix-icaza-card.avif',
+  '/img/terminal-sur/horarios-esteli-yali-estelibuses-felix-icaza-card.webp'
+]
---
+++ build/js/sw-app-shell.js
@@ -0,0 +1,148 @@
+/* eslint-disable no-unused-vars */
+
+const APP_SHELL = [
+  '/',
+  '/index.html',
+
+  '/offline.html',
+
+  '/terminal-norte-esteli',
+  '/terminal-norte-esteli/',
+  '/terminal-norte-esteli/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-achuapa',
+  '/terminal-norte-esteli/horarios-esteli-achuapa/',
+  '/terminal-norte-esteli/horarios-esteli-achuapa/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-el-regadio',
+  '/terminal-norte-esteli/horarios-esteli-el-regadio/',
+  '/terminal-norte-esteli/horarios-esteli-el-regadio/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-el-sauce',
+  '/terminal-norte-esteli/horarios-esteli-el-sauce/',
+  '/terminal-norte-esteli/horarios-esteli-el-sauce/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-jalapa',
+  '/terminal-norte-esteli/horarios-esteli-jalapa/',
+  '/terminal-norte-esteli/horarios-esteli-jalapa/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-jinotega',
+  '/terminal-norte-esteli/horarios-esteli-jinotega/',
+  '/terminal-norte-esteli/horarios-esteli-jinotega/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-la-concordia',
+  '/terminal-norte-esteli/horarios-esteli-la-concordia/',
+  '/terminal-norte-esteli/horarios-esteli-la-concordia/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-la-pita',
+  '/terminal-norte-esteli/horarios-esteli-la-pita/',
+  '/terminal-norte-esteli/horarios-esteli-la-pita/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-la-rica',
+  '/terminal-norte-esteli/horarios-esteli-la-rica/',
+  '/terminal-norte-esteli/horarios-esteli-la-rica/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-managua/',
+  '/terminal-norte-esteli/horarios-esteli-managua',
+  '/terminal-norte-esteli/horarios-esteli-managua/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-masaya',
+  '/terminal-norte-esteli/horarios-esteli-masaya/',
+  '/terminal-norte-esteli/horarios-esteli-masaya/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-ocotal',
+  '/terminal-norte-esteli/horarios-esteli-ocotal/',
+  '/terminal-norte-esteli/horarios-esteli-ocotal/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-oro-verde',
+  '/terminal-norte-esteli/horarios-esteli-oro-verde/',
+  '/terminal-norte-esteli/horarios-esteli-oro-verde/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-pueblo-nuevo',
+  '/terminal-norte-esteli/horarios-esteli-pueblo-nuevo/',
+  '/terminal-norte-esteli/horarios-esteli-pueblo-nuevo/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-quilali',
+  '/terminal-norte-esteli/horarios-esteli-quilali/',
+  '/terminal-norte-esteli/horarios-esteli-quilali/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-san-juan-de-limay',
+  '/terminal-norte-esteli/horarios-esteli-san-juan-de-limay/',
+  '/terminal-norte-esteli/horarios-esteli-san-juan-de-limay/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-san-juan-del-rio-coco',
+  '/terminal-norte-esteli/horarios-esteli-san-juan-del-rio-coco/',
+  '/terminal-norte-esteli/horarios-esteli-san-juan-del-rio-coco/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-san-marcos-de-colon',
+  '/terminal-norte-esteli/horarios-esteli-san-marcos-de-colon/',
+  '/terminal-norte-esteli/horarios-esteli-san-marcos-de-colon/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-san-rafael-del-norte',
+  '/terminal-norte-esteli/horarios-esteli-san-rafael-del-norte/',
+  '/terminal-norte-esteli/horarios-esteli-san-rafael-del-norte/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-somoto',
+  '/terminal-norte-esteli/horarios-esteli-somoto/',
+  '/terminal-norte-esteli/horarios-esteli-somoto/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-wiwili',
+  '/terminal-norte-esteli/horarios-esteli-wiwili/',
+  '/terminal-norte-esteli/horarios-esteli-wiwili/index.html',
+
+  '/terminal-norte-esteli/horarios-esteli-yali',
+  '/terminal-norte-esteli/horarios-esteli-yali/',
+  '/terminal-norte-esteli/horarios-esteli-yali/index.html',
+
+  '/terminal-sur-esteli',
+  '/terminal-sur-esteli/',
+  '/terminal-sur-esteli/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-achuapa',
+  '/terminal-sur-esteli/horarios-esteli-achuapa/',
+  '/terminal-sur-esteli/horarios-esteli-achuapa/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-el-espinal',
+  '/terminal-sur-esteli/horarios-esteli-el-espinal/',
+  '/terminal-sur-esteli/horarios-esteli-el-espinal/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-la-trinidad',
+  '/terminal-sur-esteli/horarios-esteli-la-trinidad/',
+  '/terminal-sur-esteli/horarios-esteli-la-trinidad/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-leon',
+  '/terminal-sur-esteli/horarios-esteli-leon/',
+  '/terminal-sur-esteli/horarios-esteli-leon/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-managua',
+  '/terminal-sur-esteli/horarios-esteli-managua/',
+  '/terminal-sur-esteli/horarios-esteli-managua/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-matagalpa',
+  '/terminal-sur-esteli/horarios-esteli-matagalpa/',
+  '/terminal-sur-esteli/horarios-esteli-matagalpa/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-murra',
+  '/terminal-sur-esteli/horarios-esteli-murra/',
+  '/terminal-sur-esteli/horarios-esteli-murra/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-san-nicolas',
+  '/terminal-sur-esteli/horarios-esteli-san-nicolas/',
+  '/terminal-sur-esteli/horarios-esteli-san-nicolas/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-tisey-la-estanzuela',
+  '/terminal-sur-esteli/horarios-esteli-tisey-la-estanzuela/',
+  '/terminal-sur-esteli/horarios-esteli-tisey-la-estanzuela/index.html',
+
+  '/terminal-sur-esteli/horarios-esteli-yali',
+  '/terminal-sur-esteli/horarios-esteli-yali/',
+  '/terminal-sur-esteli/horarios-esteli-yali/index.html',
+
+  '/css/lib/windi.css',
+  '/css/style.css',
+  '/js/app.js',
+  '/js/filter.js',
+  '/js/polyfill-lazyload.js',
+  '/js/search.js',
+  '/js/sw-app-shell.js'
+]
