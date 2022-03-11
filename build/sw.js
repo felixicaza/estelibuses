fba8a3c1d6992a41c71bdab244751c20b9e17022
Felix Icaza <fx.joliett17@gmail.com>
2022-03-11 16:36:54 -0600

    create cache with network fallback strategy

--- build/sw.js
+++ build/sw.js
@@ -1,5 +1,54 @@
 /* eslint-disable no-restricted-globals */
+/* eslint-disable consistent-return */
+/* eslint-disable no-undef */
+
+importScripts('/js/sw-app-shell.js')
+importScripts('/js/sw-app-shell-media.js')
+
+const CACHE_CORE = 'core-v1'
+const CACHE_MEDIA = 'media-v1'
+const CACHE_DYNAMIC = 'dynamic-v1'
+
+self.addEventListener('install', e => {
+  const setCacheCore = caches
+    .open(CACHE_CORE)
+    .then(coreCache => coreCache.addAll(APP_SHELL))
+
+  const setCacheMedia = caches
+    .open(CACHE_MEDIA)
+    .then(mediaCache => mediaCache.addAll(IMAGES_APP_SHELL))
+
+  e.waitUntil(Promise.all([setCacheCore, setCacheMedia]))
+})
+
+self.addEventListener('activate', () => {
+  caches.keys().then(keys => {
+    keys.forEach(key => {
+      if (key !== CACHE_CORE && key.includes('core')) {
+        return caches.delete(key)
+      }
+    })
+  })
+})
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request))
+  const fetchResponse = caches.match(e.request).then(response => {
+    if (response) return response
+
+    return fetch(e.request)
+      .then(newResponse => {
+        caches.open(CACHE_DYNAMIC).then(cache => {
+          cache.put(e.request, newResponse)
+        })
+
+        return newResponse.clone()
+      })
+      .catch(() => {
+        if (e.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/offline.html')
+        }
+      })
+  })
+
+  e.respondWith(fetchResponse)
 })
