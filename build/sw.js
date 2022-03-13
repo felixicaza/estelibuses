492f8fece6b7627512a3e897bf936e7e984e6991
Felix Icaza <fx.joliett17@gmail.com>
2022-03-13 09:14:23 -0600

    update cache version

--- build/sw.js
+++ build/sw.js
@@ -5,8 +5,8 @@
 importScripts('/js/sw-app-shell.js')
 importScripts('/js/sw-app-shell-media.js')
 
-const CACHE_CORE = 'core-v1.2'
-const CACHE_MEDIA = 'media-v1.2'
+const CACHE_CORE = 'core-v1.3'
+const CACHE_MEDIA = 'media-v1.3'
 const CACHE_DYNAMIC = 'dynamic-v1'
 
 self.addEventListener('install', e => {
