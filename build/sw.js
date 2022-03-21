fd2c7d49ee1d89e803c879048debe888130a16d2
Felix Icaza <fx.joliett17@gmail.com>
2022-03-21 08:17:50 -0600

    update terminal norte info

--- build/sw.js
+++ build/sw.js
@@ -5,8 +5,8 @@
 importScripts('/js/sw-app-shell.js')
 importScripts('/js/sw-app-shell-media.js')
 
-const CACHE_CORE = 'core-v2.2'
-const CACHE_MEDIA = 'media-v2.2'
+const CACHE_CORE = 'core-v2.3'
+const CACHE_MEDIA = 'media-v2.3'
 const CACHE_DYNAMIC = 'dynamic-v1'
 
 self.addEventListener('install', e => {
