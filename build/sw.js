3017ba12ffea57df8dc28711d5030993a5731d03
Felix Icaza <fx.joliett17@gmail.com>
2022-04-01 09:12:19 -0600

    add users poll

--- build/sw.js
+++ build/sw.js
@@ -5,8 +5,8 @@
 importScripts('/js/sw-app-shell.js')
 importScripts('/js/sw-app-shell-media.js')
 
-const CACHE_CORE = 'core-v2.6'
-const CACHE_MEDIA = 'media-v2.6'
+const CACHE_CORE = 'core-v2.7'
+const CACHE_MEDIA = 'media-v2.7'
 const CACHE_DYNAMIC = 'dynamic-v1'
 
 self.addEventListener('install', e => {
