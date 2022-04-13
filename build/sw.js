168ce913a37fc09e29721bbfdb03992ccec67f03
Felix Icaza <fx.joliett17@gmail.com>
2022-04-13 11:58:34 -0600

    update version

--- build/sw.js
+++ build/sw.js
@@ -5,9 +5,9 @@
 importScripts('/js/sw-app-shell.js')
 importScripts('/js/sw-app-shell-media.js')
 
-const CACHE_CORE = 'core-v2.21'
-const CACHE_MEDIA = 'media-v2.21'
-const CACHE_DYNAMIC = 'dynamic-v2.21'
+const CACHE_CORE = 'core-v2.22'
+const CACHE_MEDIA = 'media-v2.22'
+const CACHE_DYNAMIC = 'dynamic-v2.22'
 
 self.addEventListener('install', e => {
   self.skipWaiting()
