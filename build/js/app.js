b0e3f0b877578a1151afa820b462c890bced98b8
Felix Icaza <fx.joliett17@gmail.com>
2022-04-16 11:09:48 -0600

    update notification

--- build/js/app.js
+++ build/js/app.js
@@ -283,7 +283,7 @@ const containerNotification = document.getElementById('container-notification')
 const closeNotification = document.getElementById('notification-close')
 
 window.addEventListener('load', () => {
-  if (localStorage.getItem('notifynew') !== 'true') {
+  if (localStorage.getItem('tutorial') !== 'true') {
     containerNotification.style.opacity = '1'
 
     setTimeout(() => {
@@ -293,7 +293,7 @@ window.addEventListener('load', () => {
 })
 
 closeNotification.addEventListener('click', () => {
-  localStorage.setItem('notifynew', true)
+  localStorage.setItem('tutorial', true)
   containerNotification.style.transform = 'translateY(-100%)'
   containerNotification.style.opacity = '0'
 })
