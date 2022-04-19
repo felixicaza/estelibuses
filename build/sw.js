e85c610c6ffc003c4fa8d82be7e28fe6068b2bef
Felix Icaza <fx.joliett17@gmail.com>
2022-04-19 08:43:35 -0600

    optimize google ads load

--- build/sw.js
+++ build/sw.js
@@ -5,9 +5,9 @@
 importScripts('/js/sw-app-shell.js')
 importScripts('/js/sw-app-shell-media.js')
 
-const CACHE_CORE = 'core-v2.27'
-const CACHE_MEDIA = 'media-v2.27'
-const CACHE_DYNAMIC = 'dynamic-v2.27'
+const CACHE_CORE = 'core-v2.28'
+const CACHE_MEDIA = 'media-v2.28'
+const CACHE_DYNAMIC = 'dynamic-v2.28'
 
 self.addEventListener('install', e => {
   self.skipWaiting()
