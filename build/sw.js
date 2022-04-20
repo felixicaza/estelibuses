bad1d21eb3292b844d401350e0ba825beff5581b
Felix Icaza <fx.joliett17@gmail.com>
2022-04-19 21:21:54 -0600

    add new schema

--- build/sw.js
+++ build/sw.js
@@ -5,9 +5,9 @@
 importScripts('/js/sw-app-shell.js')
 importScripts('/js/sw-app-shell-media.js')
 
-const CACHE_CORE = 'core-v2.31'
-const CACHE_MEDIA = 'media-v2.31'
-const CACHE_DYNAMIC = 'dynamic-v2.31'
+const CACHE_CORE = 'core-v2.32'
+const CACHE_MEDIA = 'media-v2.32'
+const CACHE_DYNAMIC = 'dynamic-v2.32'
 
 self.addEventListener('install', e => {
   self.skipWaiting()
