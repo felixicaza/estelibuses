369ee94940c92699962492174d2c2f3366e4eba7
Felix Icaza <fx.joliett17@gmail.com>
2022-04-21 19:38:26 -0600

    small fixes

--- build/sw.js
+++ build/sw.js
@@ -5,9 +5,9 @@
 importScripts('/js/sw-app-shell.js')
 importScripts('/js/sw-app-shell-media.js')
 
-const CACHE_CORE = 'core-v2.37'
-const CACHE_MEDIA = 'media-v2.37'
-const CACHE_DYNAMIC = 'dynamic-v2.37'
+const CACHE_CORE = 'core-v2.38'
+const CACHE_MEDIA = 'media-v2.38'
+const CACHE_DYNAMIC = 'dynamic-v2.38'
 
 self.addEventListener('install', e => {
   self.skipWaiting()
