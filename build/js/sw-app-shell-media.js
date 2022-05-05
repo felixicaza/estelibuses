d8c669d4ee7802798a16c9c06cafa7d710e47a6b
Felix Icaza <fx.joliett17@gmail.com>
2022-05-05 11:04:48 -0600

    remove unused assets and update service worker app shell

--- build/js/sw-app-shell-media.js
+++ build/js/sw-app-shell-media.js
@@ -1,15 +1,6 @@
 /* eslint-disable no-unused-vars */
 
 const IMAGES_APP_SHELL = [
-  '/favicons/favicon-16x16.png',
-  '/favicons/favicon-32x32.png',
-
-  'https://res.cloudinary.com/fx-icaza/image/upload/q_auto:low,w_560/v1651427365/estelibuses-ads/banner-agencia-de-viajes-tisey-mobile.avif',
-  'https://res.cloudinary.com/fx-icaza/image/upload/q_auto:low,w_560/v1651427365/estelibuses-ads/banner-agencia-de-viajes-tisey-mobile.webp',
-
-  'https://res.cloudinary.com/fx-icaza/image/upload/q_auto:low,w_1200/v1651427365/estelibuses-ads/banner-agencia-de-viajes-tisey.avif',
-  'https://res.cloudinary.com/fx-icaza/image/upload/q_auto:low,w_1200/v1651427365/estelibuses-ads/banner-agencia-de-viajes-tisey.webp',
-
   '/img/terminal-norte-estelibuses-felix-icaza.avif',
   '/img/terminal-norte-estelibuses-felix-icaza.webp',
   '/img/terminal-norte-estelibuses-mobile-felix-icaza.avif',
