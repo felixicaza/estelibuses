08396c6928f2dda067f3ff21416b215db04fd8aa
Felix Icaza <fx.joliett17@gmail.com>
2022-05-27 21:57:38 -0600

    small fixes

--- build/sw.js
+++ build/sw.js
@@ -11,9 +11,9 @@ importScripts(
 workbox.setConfig({ debug: false })
 workbox.googleAnalytics.initialize()
 
-const CACHE_CORE = 'core-v2.54'
-const CACHE_MEDIA = 'media-v2.54'
-const CACHE_DYNAMIC = 'dynamic-v2.54'
+const CACHE_CORE = 'core-v2.55'
+const CACHE_MEDIA = 'media-v2.55'
+const CACHE_DYNAMIC = 'dynamic-v2.55'
 
 self.addEventListener('install', e => {
   const setCacheCore = caches
