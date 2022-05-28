9d4bccebf4431a0907bf3572cc221d230bce3851
Felix Icaza <fx.joliett17@gmail.com>
2022-05-27 22:04:34 -0600

    fix contact input bg color

--- build/sw.js
+++ build/sw.js
@@ -11,9 +11,9 @@ importScripts(
 workbox.setConfig({ debug: false })
 workbox.googleAnalytics.initialize()
 
-const CACHE_CORE = 'core-v2.55'
-const CACHE_MEDIA = 'media-v2.55'
-const CACHE_DYNAMIC = 'dynamic-v2.55'
+const CACHE_CORE = 'core-v2.56'
+const CACHE_MEDIA = 'media-v2.56'
+const CACHE_DYNAMIC = 'dynamic-v2.56'
 
 self.addEventListener('install', e => {
   const setCacheCore = caches
