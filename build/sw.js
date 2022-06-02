79535cafa65c20cc21086b372968233fe7147d81
Felix Icaza <fx.joliett17@gmail.com>
2022-06-02 16:28:52 -0600

    disable alert

--- build/sw.js
+++ build/sw.js
@@ -11,9 +11,9 @@ importScripts(
 workbox.setConfig({ debug: false })
 workbox.googleAnalytics.initialize()
 
-const CACHE_CORE = 'core-v2.60'
-const CACHE_MEDIA = 'media-v2.60'
-const CACHE_DYNAMIC = 'dynamic-v2.60'
+const CACHE_CORE = 'core-v2.61'
+const CACHE_MEDIA = 'media-v2.61'
+const CACHE_DYNAMIC = 'dynamic-v2.61'
 
 self.addEventListener('install', e => {
   const setCacheCore = caches
