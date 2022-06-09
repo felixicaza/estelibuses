297cbe45cb0344933b25ef30c952f71ac949b51e
Felix Icaza <fx.joliett17@gmail.com>
2022-06-09 17:12:58 -0600

    update cache version

--- build/sw.js
+++ build/sw.js
@@ -11,9 +11,9 @@ importScripts(
 workbox.setConfig({ debug: false })
 workbox.googleAnalytics.initialize()
 
-const CACHE_CORE = 'core-v2.62'
-const CACHE_MEDIA = 'media-v2.62'
-const CACHE_DYNAMIC = 'dynamic-v2.62'
+const CACHE_CORE = 'core-v2.63'
+const CACHE_MEDIA = 'media-v2.63'
+const CACHE_DYNAMIC = 'dynamic-v2.63'
 
 self.addEventListener('install', e => {
   const setCacheCore = caches
