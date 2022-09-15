b078c1d6bf1337c6d5715c635800db2432cf5fb0
Felix Icaza <fx.joliett17@gmail.com>
2022-09-15 08:59:02 -0600

    chore: configure astro options

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
+const website = 'https://estelibuses.web.app'
+
 // https://astro.build/config
 export default defineConfig({
+  site: website,
+  trailingSlash: 'never',
+  server: {
+    host: true
+  },
   integrations: [tailwind()]
 })
