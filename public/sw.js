76ee491bc4da3fe8a28d1faadc5ae51ff726ad6b
Felix Icaza <fx.joliett17@gmail.com>
2022-10-25 02:15:51 +0000

    chore: update changes

--- public/sw.js
+++ public/sw.js
@@ -14,9 +14,9 @@ importScripts(
 workbox.setConfig({ debug: false })
 workbox.googleAnalytics.initialize()
 
-const CACHE_CORE = 'core-v2.80'
-const CACHE_MEDIA = 'media-v2.80'
-const CACHE_DYNAMIC = 'dynamic-v2.80'
+const CACHE_CORE = 'core-v2.81'
+const CACHE_MEDIA = 'media-v2.81'
+const CACHE_DYNAMIC = 'dynamic-v2.81'
 
 self.addEventListener('install', (e) => {
   const setCacheCore = caches
