3b05ef29e05a2084f27d45c321e51d662e59e1e7
Felix Icaza <fx.joliett17@gmail.com>
2022-11-01 22:22:02 +0000

    feat: create greetings component

--- public/sw.js
+++ public/sw.js
@@ -13,9 +13,9 @@ importScripts(
 workbox.setConfig({ debug: false })
 workbox.googleAnalytics.initialize()
 
-const CACHE_CORE = 'core-v2.82'
-const CACHE_MEDIA = 'media-v2.82'
-const CACHE_DYNAMIC = 'dynamic-v2.82'
+const CACHE_CORE = 'core-v2.83'
+const CACHE_MEDIA = 'media-v2.83'
+const CACHE_DYNAMIC = 'dynamic-v2.83'
 
 self.addEventListener('install', (e) => {
   const setCacheCore = caches
