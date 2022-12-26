4bb257cfc1ae2f960135f5f7fe93d054739aec10
Felix Icaza <fx.joliett17@gmail.com>
2022-12-26 14:55:32 +0000

    chore: update contact page and notifications

--- public/sw.js
+++ public/sw.js
@@ -13,8 +13,8 @@ importScripts(
 workbox.setConfig({ debug: false })
 workbox.googleAnalytics.initialize()
 
-const CACHE_CORE = 'core-v2.87'
-const CACHE_DYNAMIC = 'dynamic-v2.86'
+const CACHE_CORE = 'core-v2.88'
+const CACHE_DYNAMIC = 'dynamic-v2.87'
 
 self.addEventListener('install', (e) => {
   const setCacheCore = caches
