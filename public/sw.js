d5a2f24ff58fe7486d00e135a932581f6f53df89
Felix Icaza <fx.joliett17@gmail.com>
2023-01-02 14:11:44 +0000

    chore: remove notification

--- public/sw.js
+++ public/sw.js
@@ -13,8 +13,8 @@ importScripts(
 workbox.setConfig({ debug: false })
 workbox.googleAnalytics.initialize()
 
-const CACHE_CORE = 'core-v2.88'
-const CACHE_DYNAMIC = 'dynamic-v2.87'
+const CACHE_CORE = 'core-v2.89'
+const CACHE_DYNAMIC = 'dynamic-v2.88'
 
 self.addEventListener('install', (e) => {
   const setCacheCore = caches
