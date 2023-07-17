c41293d0a573109f8ae5cc5e480f8f3d73210e51
Felix Icaza <fx.joliett17@gmail.com>
2023-07-17 10:21:09 -0600

    chore: update service worker cache version

--- public/sw.js
+++ public/sw.js
@@ -13,8 +13,8 @@ importScripts(
 workbox.setConfig({ debug: false })
 workbox.googleAnalytics.initialize()
 
-const CACHE_CORE = 'core-v2.96'
-const CACHE_DYNAMIC = 'dynamic-v2.96'
+const CACHE_CORE = 'core-v2.97'
+const CACHE_DYNAMIC = 'dynamic-v2.97'
 
 self.addEventListener('install', (e) => {
   const setCacheCore = caches
