2035cedd225a74b69c8555ed428a99ef8ab0e0f7
Felix Icaza <fx.joliett17@gmail.com>
2025-05-03 18:05:20 -0600

    fix: enhance type checking with astro check

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,2 @@
 declare module 'astro-imagetools';
 declare module 'astro-imagetools/components';
-
-declare namespace astroHTML.JSX {
-  interface SVGAttributes {
-    'stroke-linejoin'?:
-      | 'arcs'
-      | 'miter'
-      | 'round'
-      | 'bevel'
-      | 'inherit'
-      | undefined
-      | null
-  }
-}
